9085dc9b8bba90dc493ec52e1bca9a3958ac7eb9
Ben Johnson <benjamin.johnson@kraftfoods.com>
2012-03-27 22:26:34 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KraftMswApp::Application.config.secret_token = '468927232e3b1aa829dfbd90f6502d5ad2cd44f6d35fafb2ff8afc8cad5fd4ef8580575df11fc6a37dfe18bb2c8c8c7badc67f659f57cc55dd105b1642cc1571'
